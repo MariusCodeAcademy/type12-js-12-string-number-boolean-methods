db8c29a07c00552aaa865cc9e07a1a20652fa385
Marius <marius@example.com>
2022-07-21 15:16:17 +0300

    uzd

---
+++ js/number.js
@@ -0,0 +1,11 @@
+//https://www.w3schools.com/js/js_number_methods.asp
+
+const num1 = 50;
+const num2 = 14.5;
+
+// number.toFixed(2)
+
+// parasyti funkcija kuri gavusi skaiciu grazina valueta eurais
+// makeMoney() paduodam 50 gaunam  €50.00
+
+// patobulinti funkcija kad galetume ivesti valiutos simboli
