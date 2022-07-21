b65af76be45f4c95b51c918969aac77c4fd4358b
Marius <marius@example.com>
2022-07-21 14:57:34 +0300

    regular expression regex

--- js/string.js
+++ js/string.js
@@ -1,7 +1,7 @@
 'use strict';
 console.log('main.js');
 
-const str1 = 'Brown fox jumped over the lazy dog.';
+let str1 = 'Brown fox jumped over the lazy dog.';
 // str.slice(nuoIndex, ikiIndex) =====================================
 
 // gauti 'Brown'
@@ -35,3 +35,49 @@ console.log('str1 ===', str1);
 
 const str2 = str1.concat(' ', 'Sun', ' ', 'is ', 'shining.');
 console.log('str2 ===', str2);
+
+// str.charAt(index) - grazina simboli toje vietoje
+
+const str3 = 'sun is shining';
+// gauti 'i'
+const i = str3.charAt(4);
+
+// gauti str3 sentence case - pirma raide didziaja
+
+const str2Sentence = str3.charAt(0).toUpperCase() + str3.slice(1);
+
+// parasyti funkcija kuri argumentu gauna stringa ir grazina pirmaja raide padidinta.
+
+/**
+ * this function makes first letter upper case
+ * @param {string} str
+ * @return {string}
+ */
+function makeSentenceCase(str) {
+  const senC = str.charAt(0).toUpperCase() + str.slice(1);
+  console.log('makeSentenceCase ===', senC);
+  return senC;
+}
+
+makeSentenceCase(str3);
+const upper = makeSentenceCase('forgot to upper case');
+
+// str.split(per ka padalinti) -
+
+const str1Arr = str1.split(' ');
+console.log('str1Arr ===', str1Arr);
+
+let capitalizedSentence = '';
+for (const word of str1Arr) {
+  capitalizedSentence += makeSentenceCase(word) + ' ';
+}
+console.log('capitalizedSentence ===', capitalizedSentence);
+
+console.log('str1Arr.join(" ** ") ===', str1Arr.join(' '));
+
+// regular expression regex
+
+str1 = 'Brown fox jumped over the fox lazy dog.';
+
+const sqrl = str1.replace(/FOX/gi, 'squrell');
+console.log('sqrl ===', sqrl);
